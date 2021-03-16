b60e575568756728ba131047d6eac8123bdddecb
PiterM <piotr.markiewicz@ttms.pl>
2021-03-16 11:36:39 +0100

    cookies consent message

--- src/app/wrapper.tsx
+++ src/app/wrapper.tsx
@@ -2,12 +2,24 @@ import * as React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Player from '../containers/player/player';
+import CookieConsent from 'react-cookie-consent';
 
 export default ({ element }: any) => {
   return (
-    <Provider store={store}>
-        {element}
-        <Player />
-    </Provider>
+    <>
+      <Provider store={store}>
+          {element}
+          <Player />
+      </Provider>
+      <CookieConsent
+        location="bottom"
+        cookieName="cookieConsent"
+        expires={10000}
+        style={{ fontSize: '14px'}}
+        buttonStyle={{ fontSize: '14px'}}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+    </>
   );
 };
